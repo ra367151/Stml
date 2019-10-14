a9c1051a39d6513fea8f0cb5c97c109bf6f83dfe
weiwxg <weiwxg@126.com>
2019-10-14 17:54:13 +0800

    Login、Register页面

--- src/Stml.Application/Dtos/Inputs/UserRegisterInput.cs
+++ src/Stml.Application/Dtos/Inputs/UserRegisterInput.cs
@@ -8,19 +8,23 @@ namespace Stml.Application.Dtos.Inputs
     public class UserRegisterInput
     {
         [Display(Name = "账号")]
+        [Required]
         public string UserName { get; set; }
 
         [Display(Name = "邮箱")]
         [DataType(DataType.EmailAddress)]
+        [Required]
         public string Email { get; set; }
 
         [Display(Name = "密码")]
         [DataType(DataType.Password)]
+        [Required]
         public string Password { get; set; }
 
         [Display(Name = "确认密码")]
         [Compare(nameof(Password))]
         [DataType(DataType.Password)]
+        [Required]
         public string ConfirmPassword { get; set; }
     }
 }
