a44331d144bfdb09be30b582204935a07e800893
weiwxg <weiwxg@126.com>
2019-10-22 09:13:14 +0800

    add toastr

--- src/Stml.Web/wwwroot/js/site.js
+++ src/Stml.Web/wwwroot/js/site.js
@@ -1,3 +1,9 @@
 ﻿; (function ($) {
+    var funcs = {
+        cfgToastr: function (options) {
+            options.positionClass = 'toast-bottom-right';
+        }
+    };
 
+    funcs.cfgToastr(toastr.options);
 })(jQuery);
